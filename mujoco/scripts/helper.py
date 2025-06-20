4123687befeca36d92b272144589eacb79f31fdb
Khaledwahba1994 <Khaled.hassan1994@gmail.com>
2025-06-20 15:38:30 +0200

    update helper script

--- mujoco/scripts/helper.py
+++ mujoco/scripts/helper.py
@@ -1,5 +1,6 @@
 import yaml
 import numpy as np
+import rowan as rn
 
 def saveyaml(file_out, data):
     with open(file_out, "w") as f:
@@ -14,3 +15,20 @@ def loadyaml(file_in):
 def loadcsv(filename):
     return np.loadtxt(filename, delimiter=",", skiprows=1, ndmin=2)
 
+
+def derivative(vec, dt):
+    dvec = []
+    # dvec  =[[0,0,0]]
+    for i in range(len(vec) - 1):
+        dvectmp = (vec[i + 1] - vec[i]) / dt
+        dvec.append(dvectmp.tolist())
+    dvec.append([0, 0, 0])
+    return np.asarray(dvec)
+
+
+def reorder_quat(quat):
+    """
+    Reorder quaternion from [x, y, z, w] to [w, x, y, z]
+    """
+    quat_reordered =  np.array([quat[3], quat[0], quat[1], quat[2]])
+    return quat_reordered
